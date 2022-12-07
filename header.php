df2d6029e59ebaca239815b44760de7b8ba9645d
Ammar Shah <syedammarali24@gmail.com>
2022-12-07 22:56:49 +0500

    Lots of small changes fixed

--- header.php
+++ header.php
@@ -5,13 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title><?php echo get_the_title(); ?> - <?php echo get_bloginfo( 'name' ); ?> - <?php echo get_bloginfo( 'description' ); ?> </title>
 
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="32x32"
-      href="<?php echo get_template_directory_uri(); ?>/<?php echo get_template_directory_uri(); ?>/assets/images/favicons/favicon.png"
-    />
-
     <meta name="description" content="" />
 
     <!-- fonts -->
